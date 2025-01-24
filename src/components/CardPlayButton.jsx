c32642a20ae462210bd58a5f04a268ab48b4790d
Angel Escobar <angelescobar512@gmail.com>
2025-01-24 14:36:07 -0500

    Se agrega la tabla y el mapeo de canciones

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,7 +2,7 @@
 import { Pause,Play } from "./Player";
 import { usePlayerStore } from "@/store/playerStore";
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
     const { 
         currentMusic, 
         isPlaying, 
@@ -42,11 +42,11 @@ export function CardPlayButton({ id }) {
     }
 
  
-
+const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
 
   return (
-    <button onClick={handleClick} className="card-play-button rounded-full text-black bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
-        {isPlayingPlaylist ? <Pause /> : <Play />}
+    <button onClick={handleClick} className="card-play-button rounded-full text-black bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400 ">
+        {isPlayingPlaylist ? <Pause className={iconClassName}/> : <Play className={iconClassName}/>}
         
     </button>
     
